a491f0cda40c9c387c05b9e29f5f621eeaa9bfb0
vijai021096 <vijai021096@gmail.com>
2020-07-13 11:24:10 +0530

    commit

--- ExpenseTracker/ExpenseTracker/src/main/java/com/luv2code/spring/ExpenseTracker/Repository/ConfirmationTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luv2code.spring.ExpenseTracker.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.luv2code.spring.ExpenseTracker.Model.ConfirmationToken;
-
-public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Integer> {
-	ConfirmationToken findByConfirmationToken(String confirmationToken);
-}
